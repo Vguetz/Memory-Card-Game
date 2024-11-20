ede8e90f8f171e34dbabca2e1308b5fbe54cb831
Vguetz <gomezlucas712@gmail.com>
2024-11-20 10:14:14 -0300

    Configurar despliegue en GitHub Pages y rutas de imágenes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/", // Añadir la barra diagonal final
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/Memory-Card-Game/" : "/", // Configuración condicional del base
+  };
 });
